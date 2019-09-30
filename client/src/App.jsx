42d609dbfb680f6a2476bbad3145ece8f9fa92bc
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-09-29 22:54:32 -0600

    Add React boilerplate for client

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => (
+  <>
+    <h1>Welcome to the mess hall!</h1>
+    <p>Hard to get a more useless messaging app than this.</p>
+  </>
+)
+
+export { App }
