634679f6dcf164e3870d9a1e3fe790778006ea4f
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-09-30 09:13:40 -0600

    Set up basic front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import socketIO from 'socket.io-client'
 
-const App = () => (
-  <>
-    <h1>Welcome to the mess hall!</h1>
-    <p>Hard to get a more useless messaging app than this.</p>
-  </>
-)
+import { Event } from 'server/src/Event'
+
+const App = () => {
+  const io = socketIO('ws://localhost:4001/')
+
+  // State fields
+  const [username, setUsername] = useState('')
+  const [message, setMessage] = useState('')
+  const [chatLog, setChatLog] = useState([])
+
+  // Setters
+  // username
+  const changeUsername = (evt) => setUsername(evt.target.value)
+  // message
+  const changeMessage = (evt) => setMessage(evt.target.value)
+  const sendMessage = (evt) => {
+    evt.preventDefault()
+    io.emit(Event.MESSAGE, {
+      id: Math.random(), // lol
+      author: username,
+      body: message,
+    })
+    setMessage('')
+  }
+  // messages
+  const addMessage = message => setChatLog([...chatLog, message])
+
+  // Listeners
+  io.on(Event.MESSAGE, (message) => {
+    addMessage(message)
+  })
+
+  return (
+    <>
+      <ul id="chatLog">
+        {chatLog.map(message => (
+          <li key={message.id}>
+            {message.author}: {message.body}
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <div>
+        <div>
+          <label htmlFor="username">Username{' '}</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={changeUsername}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message{' '}</label>
+          <input
+            type="text"
+            name="message"
+            value={message}
+            onChange={changeMessage}
+          />
+        </div>
+        <br/>
+        <button onClick={sendMessage}>
+          Send
+        </button>
+      </div>
+    </>
+  )
+}
 
 export { App }
--- server/src/app.js
+++ server/src/app.js
@@ -18,20 +18,17 @@ const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
-// socket.io
+// socket.socketIO
 const io = socketIO(server)
 io.on(Event.CONNECT, (client) => {
-  io.emit(Event.CONNECT, client)
   console.log(`Client ${client.id} connected`)
 
   client.on(Event.MESSAGE, (message) => {
     io.emit(Event.MESSAGE, message)
-    console.log(`Received message:`)
-    console.table(message)
+    console.log(`Client ${client.id} sent message:`, message)
   })
 
   client.on(Event.DISCONNECT, () => {
-    io.emit(Event.DISCONNECT, client)
     console.log(`Client ${client.id} disconnected`)
   })
 })
